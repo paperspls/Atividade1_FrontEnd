d6075d994147598768de4f5d10fb5a3ca2e90fc9
Rhyan Magalhães <rjobdg@outlook.com>
2023-10-19 15:00:43 -0300

    Validação JScript

--- site jogos/script/script.js
+++ site jogos/script/script.js
@@ -1,36 +1,60 @@
 let nome = window.document.getElementById('nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
 
-function validarNome() { 
+
+
+nome.style.width = '100%'
+email.style.width = '100%'
+
+function validaNome() {
     let txt = document.querySelector('#txtNome')
-    if (nome.value.length < 3){
-        txt.innerHTML = 'Nome Inválido'
+    if (nome.value.length < 3) {
+        txt.innerHTML = 'nome inválido'
         txt.style.color = 'red'
     } else {
-        txt.innerHTML = 'Nome Válido'
+        txt.innerHTML = 'nome válido'
         txt.style.color = 'green'
+        nomeOk = true
+
     }
+
 }
 
-function validarEmail() { 
+function validaEmail() {
     let txtEmail = document.querySelector('#txtEmail')
-    if (email.value.indexOf ('@') == -1 || email.value.indexOf ('.com') == -1) {
-        txtEmail.innerHTML = 'Email Inválido'
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML = 'E-mail inválido'
         txtEmail.style.color = 'red'
     } else {
-        txtEmail.innerHTML = 'Email Válido'
+        txtEmail.innerHTML = 'E-mail válido'
         txtEmail.style.color = 'green'
+        emailOk = true
+
+    }
+
+}
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    if (assunto.value.length >= 100) {
+        txtAssunto.innerHTML = 'Texto é muito grande, digite no máximo 100 caracteres'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    } else {
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
     }
 }
 
-function validarAssunto() { 
-    let txtAssunto = document.querySelector('#txtAssunto')
-    if (assunto.value.length < 5) {
-        txtAssunto.innerHTML = 'Assunto Inválido'
-        txtAssunto.style.color = 'red'
+function enviar() {
+    if (nomeOk == true && emailOk == true && assuntoOk == true) {
+        alert('Formulário enviado com sucesso!')
     } else {
-        txtAssunto.innerHTML = 'Assunto Válido'
-        txtAssunto.style.color = 'green'
+        alert('Preencha o formulário correntamente antes de enviar...')
     }
 }
